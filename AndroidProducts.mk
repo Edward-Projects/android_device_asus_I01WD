5e81b5e05c655905a4102afed28f2620f07e37d5
Edward Wu <bluehome.wu@gmail.com>
2022-04-13 00:48:09 +0800

    zenfone6: Initial Lineage-OS bringup
    Signed-off-by: Edward Wu <bluehome.wu@gmail.com>

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,8 +1,8 @@
 PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/device.mk \
-    $(LOCAL_DIR)/omni_zenfone6.mk
+    $(LOCAL_DIR)/lineage_zenfone6.mk
 
 COMMON_LUNCH_CHOICES := \
-    omni_zenfone6-user \
-    omni_zenfone6-userdebug \
-    omni_zenfone6-eng
+    lineage_zenfone6-user \
+    lineage_zenfone6-userdebug \
+    lineage_zenfone6-eng
